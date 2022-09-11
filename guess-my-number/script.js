0d95d2fc8c889924fa3c5f91cfdecc0d25e26feb
Mike <spormuv@gmail.com>
2022-09-11 12:05:27 +0300

    feat: implement manipulating CSS styles

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -18,10 +18,19 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = +document.querySelector('.guess').value;
   console.log(guess, typeof guess);
 
+  // when there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No number';
+
+    // when player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉Correct Number!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    //when guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too high!';
@@ -31,6 +40,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '🔥 You lost the game!';
       document.querySelector('.score').textContent = 0;
     }
+
+    //when guess is too low
   } else {
     if (score > 1) {
       document.querySelector('.message').textContent = '📉 Too low!';
