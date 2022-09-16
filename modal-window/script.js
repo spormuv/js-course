e032224b581e7f99af2418706642a42ac6baa327
Mike <spormuv@gmail.com>
2022-09-16 15:32:43 +0300

    feat: add on-click close modal

--- modal-window/script.js
+++ modal-window/script.js
@@ -5,10 +5,14 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnsOpenModal);
+const closeModal = () => {
+  modal.classList.toggle('hidden');
+  overlay.classList.toggle('hidden');
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', () => {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+  btnsOpenModal[i].addEventListener('click', closeModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
