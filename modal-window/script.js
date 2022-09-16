5ab1f684944674ada4b475c43558cb1277d2dab5
Mike <spormuv@gmail.com>
2022-09-16 14:32:43 +0300

    feat: add appearance of a modal

---
+++ modal-window/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
