cd2113a196f34ce37490a58978cc37bb48300ea9
Mike <spormuv@gmail.com>
2022-09-16 18:17:34 +0300

    feat: add on-esc close modal

--- modal-window/script.js
+++ modal-window/script.js
@@ -5,14 +5,24 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
 const closeModal = () => {
-  modal.classList.toggle('hidden');
-  overlay.classList.toggle('hidden');
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 };
 
 for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', closeModal);
+  btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
-
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
