410b852b95e08f17a04f178658bb543d16412f99
Mike <spormuv@gmail.com>
2023-01-28 18:42:54 +0300

    feat: refactor code with MVC pattern

--- forkify_app/src/js/controller.js
+++ forkify_app/src/js/controller.js
@@ -1,3 +1,9 @@
+import * as model from './model';
+import recipeView from './views/recipeView';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -7,3 +13,28 @@ const timeout = function (s) {
     }, s * 1000);
   });
 };
+
+/////////////////////////////////////////////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // 1) loading recipe
+    await model.loadRecipe(id);
+    // 2) rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+['hashchange', 'load'].forEach(ev =>
+  window.addEventListener(ev, controlRecipes)
+);
+
+// window.addEventListener('hashchange', controlRecipes);
+// window.addEventListener('load', controlRecipes);
