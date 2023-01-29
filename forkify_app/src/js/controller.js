2509c13ace81fd0ead131cec55f612162b22fe01
Mike <spormuv@gmail.com>
2023-01-29 13:06:59 +0300

    feat: implement render recipe

--- forkify_app/src/js/controller.js
+++ forkify_app/src/js/controller.js
@@ -5,36 +5,23 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
 const recipeContainer = document.querySelector('.recipe');
-
-const timeout = function (s) {
-  return new Promise((_, reject) => {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
 /////////////////////////////////////////////////////////////////////////////
 
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
-    console.log(id);
     if (!id) return;
     recipeView.renderSpinner();
-
     // 1) loading recipe
     await model.loadRecipe(id);
     // 2) rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    alert(err);
+    recipeView.renderError();
   }
 };
 
-['hashchange', 'load'].forEach(ev =>
-  window.addEventListener(ev, controlRecipes)
-);
-
-// window.addEventListener('hashchange', controlRecipes);
-// window.addEventListener('load', controlRecipes);
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+init();
